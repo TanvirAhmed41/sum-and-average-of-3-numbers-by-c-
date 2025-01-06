49cff350a1ab3b35717b3463ed32790bd33d9812
Tanvir <santoahmed225@gmail.com>
2025-01-06 11:05:32 +0600

    circle area calculator

--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@ class Test
 {
     public static void Main (string[] args){
 
-        int num1,num2,num3,sum;
+     /*   int num1,num2,num3,sum;
         double average;
-        Console.WriteLine("Enter First number: ");
+        Console.WriteLine("Enter first number: ");
         num1 =Convert.ToInt32( Console.ReadLine());
 
         Console.WriteLine("Enter Second number: ");
@@ -16,12 +16,23 @@ class Test
         Console.WriteLine("Enter Third number: ");
         num3 = Convert.ToInt32( Console.ReadLine());
 
-        sum = num1 + num2 + num3;
+        sum = num1 + num2 + num3 ;
         Console.WriteLine($"sum = {sum}");
 
         average = sum/3;
         Console.WriteLine($"average = {average}");
-        
+        */
+
+        double radius;
+        double circleArea;
+
+        Console.WriteLine ("Circle Area Calculator");
+
+        Console.Write("Enter the value of Radius: ");
+        radius =Convert.ToDouble(Console.ReadLine());
+
+        circleArea = 3.1416 * (radius * radius);
+        Console.WriteLine($"circleArea = {circleArea.ToString("F2")}");
 
 }
 }
