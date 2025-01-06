cb570c892ae15713352ad831060901d70b1a4019
Tanvir <santoahmed225@gmail.com>
2025-01-06 10:39:30 +0600

    sum and average of 3 numbers is done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+class Test
+{
+    public static void Main (string[] args){
+
+        int num1,num2,num3,sum;
+        double average;
+        Console.WriteLine("Enter First number: ");
+        num1 =Convert.ToInt32( Console.ReadLine());
+
+        Console.WriteLine("Enter Second number: ");
+        num2 = Convert.ToInt32( Console.ReadLine());
+
+        Console.WriteLine("Enter Third number: ");
+        num3 = Convert.ToInt32( Console.ReadLine());
+
+        sum = num1 + num2 + num3;
+        Console.WriteLine($"sum = {sum}");
+
+        average = sum/3;
+        Console.WriteLine($"average = {average}");
+        
+
+}
+}
+
+
